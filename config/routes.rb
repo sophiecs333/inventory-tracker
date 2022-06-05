316a3f4322c2e241ebdca1116b77eb3a12bd5809
sophie sorin <sophie4489@hotmail.fr>
2022-06-05 20:41:53 +0200

    changed intems index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'pages#home'
+  get "items", to: "items#index"
 
   resources :warehouses, only: [:index, :show, :new, :create,] do
     # resources :items
-
     get    "items",          to: "items#index"
 
     get    "items/new",      to: "items#new",  as: :new_item
